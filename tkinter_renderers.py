cb36573149ccfd48e0dc5c93345ba2fae63019fe
IvanRomero03 <65189646+IvanRomero03@users.noreply.github.com>
2021-10-09 17:35:02 -0700

    Update tkinter_renderers.py
    Hice q el texto del menú tuviera el mismo fondo que la ventana y también agregué un botón de salir, ese botón se puede replicar si es necesario sin ningún problema.

--- tkinter_renderers.py
+++ tkinter_renderers.py
@@ -1,19 +1,31 @@
 import tkinter as tk
 
+
 def do_menu(win):
 
     menu_fr = tk.Frame(win, bg="#7BD09E")
     menu_fr.pack()
 
+    texo2 = tk.StringVar()
+    texo2.set("Pregunta2")
+    texto = tk.Label(menu_fr, textvariable=texo2, font=(
+        'Helvetica bold', 60), background="#7BD09E")
+    texto.pack()
+
     ltext = tk.StringVar()
     ltext.set("Menu")
-    tit = tk.Label(menu_fr, textvariable=ltext, font=('Helvetica bold', 40))
+    tit = tk.Label(menu_fr, textvariable=ltext, font=(
+        'Helvetica bold', 40), background="#7BD09E")
     tit.pack()
 
-    but1 = tk.Button(menu_fr, text="Iniciar juego", font=('Helvetica bold',40), fg="#FF0000")
+    but1 = tk.Button(menu_fr, text="Iniciar juego",
+                     font=('Helvetica bold', 40), fg="#FF0000")
     but1.pack()
-
+    BotonQuitar = tk.Button(menu_fr, text="Salir",
+                            font=('Helvetica bold', 40), fg="#FF0000", command=win.destroy)
+    BotonQuitar.pack()
     return menu_fr
+
 
 def do_game(win):
 
@@ -25,7 +37,8 @@ def do_game(win):
     for i in range(4):
         ltext = tk.StringVar()
         ltext.set(resp[i])
-        tit = tk.Label(game_fr, textvariable=ltext, font=('Helvetica bold', 40))
+        tit = tk.Label(game_fr, textvariable=ltext,
+                       font=('Helvetica bold', 40))
         tit.pack()
 
     return game_fr
