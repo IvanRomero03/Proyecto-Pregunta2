31e395b799049ddde43bcb4d0fc87cafdb62f9c8
IvanRomero03 <65189646+IvanRomero03@users.noreply.github.com>
2021-10-09 17:33:45 -0700

    Update main.py
    Experimenté ahí un poco con q hiciste y todo, tmbn inicialicé la ventana diréctamente en el menú principal

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import tkinter as tk
 import tkinter_renderers as tkrd
 
-#reads archivos y declaraciones
+# reads archivos y declaraciones
 win = tk.Tk()
 win.geometry("600x600")
 win.config(bg="#7BD09E")
 
-#funciones
+# inicializar en el menu inicial
+menuinicial = tkrd.do_menu(win)
+menuinicial.pack()
+
+# funciones
+
+
 def juego():
     on_game = True
     while on_game:
@@ -16,27 +22,30 @@ def juego():
             on_game = not on_game
         print("")
 
+
 def sw_fr1():
     for ele in win.winfo_children():
         ele.destroy()
     f1 = tkrd.do_menu(win)
     f1.pack()
-    
+
+
 def sw_fr2():
     for ele in win.winfo_children():
         ele.destroy()
     f2 = tkrd.do_game(win)
     f2.pack()
 
-but = tk.Button(win, text="F1", font=('Helvetica bold',40), fg="#FF0000", command=sw_fr1)
-but.pack()
 
-but2 = tk.Button(win, text="F2", font=('Helvetica bold',40), fg="#FF0000", command=sw_fr2)
-but2.pack()
+# but = tk.Button(win, text="F1", font=('Helvetica bold', 40),fg="#FF0000", command=sw_fr1)
+# but.pack()
 
-#call_renderers
+# but2 = tk.Button(win, text="F2", font=('Helvetica bold', 40),fg="#FF0000", command=sw_fr2)
+# but2.pack()
+
+# call_renderers
 sw = 0
 
-#main loop
+# main loop
 win.mainloop()
 print("Fin render")
